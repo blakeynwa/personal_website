7e4e27ebdc47908cdcacbd5e7e87579da90d8563
Blake Meyer <blake.ynwa@gmail.com>
2017-12-20 10:35:37 -0800

    Play around with react bootstrap grids

--- src/containers/Headshot.js
+++ src/containers/Headshot.js
@@ -2,15 +2,18 @@ import React from 'react';
 import img from '../images/headshot.jpg';
 import Grid from 'react-bootstrap/lib/Grid';
 import Col from 'react-bootstrap/lib/Col';
+import Row from 'react-bootstrap/lib/Row';
 import Image from 'react-bootstrap/lib/Image';
 
 
 const Headshot = () => {
   return (
     <Grid>
-      <Col>
-        <Image src={img} width={449.28} height={299.52} rounded />
-      </Col>
+      <Row>
+        <Col md={4}>
+          <Image src={img} width={449.28} height={299.52} rounded />
+        </Col>
+      </Row>
     </Grid>
   );
 };
