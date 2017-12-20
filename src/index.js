ef82b7bbfa21392062d1e426b9bac3c12ab37ed1
Blake Meyer <blake.ynwa@gmail.com>
2017-12-19 19:41:19 -0800

    Create better organizaion pattern for app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import Header from './Header';
 import Image from './Image'
 import registerServiceWorker from './registerServiceWorker';
