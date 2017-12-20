f26f7e0b4018cf271dbff681eba2260a4fe6648c
Blake Meyer <blake.ynwa@gmail.com>
2017-12-20 15:37:44 -0800

    Continue styling with bootstrap

--- src/containers/Headshot.js
+++ src/containers/Headshot.js
@@ -4,14 +4,17 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
 import Image from 'react-bootstrap/lib/Image';
+import './Headshot.css'
 
 
 const Headshot = () => {
   return (
     <Grid>
-      <Row>
-        <Col md={4}>
-          <Image src={img} width={449.28} height={299.52} rounded />
+      <Row className="checking">
+        <Col md={4} mdPush={4} className="checkingtwo">
+          <div className="portrait">
+            <Image className="img-responsive" src={img} width={449.28} height={299.52} rounded />
+          </div>
         </Col>
       </Row>
     </Grid>
