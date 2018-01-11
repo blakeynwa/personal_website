f66e5abc997fc8c39c1371a0a912b515a67ccef1
Blake Meyer <blake.ynwa@gmail.com>
2018-01-10 19:10:41 -0800

    Change image from square to circle

--- src/containers/Headshot.js
+++ src/containers/Headshot.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import img from '../images/headshot.jpg';
+import img from '../images/headshot.png';
 import Grid from 'react-bootstrap/lib/Grid';
 import Col from 'react-bootstrap/lib/Col';
 import Row from 'react-bootstrap/lib/Row';
@@ -11,9 +11,9 @@ const Headshot = () => {
   return (
     <Grid>
       <Row className="headshot-border">
-        <Col sm={8} md={4} mdPush={4} smPush={2} className="checkingtwo">
+        <Col sm={8} md={4} mdPush={4} smPush={2}>
           <div className="portrait">
-            <Image className="img-responsive" src={img} width={449.28} height={299.52} rounded />
+            <Image className="img-responsive" src={img} width={449.28} height={299.52} />
           </div>
         </Col>
       </Row>
