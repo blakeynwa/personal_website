5f3b29b7e501726620bbc2463546e9327fa7c0f8
Blake Meyer <blake.ynwa@gmail.com>
2018-01-16 10:42:38 -0800

    Make progress with aligning project names with descriptions divs

--- src/containers/Portfolio.js
+++ src/containers/Portfolio.js
@@ -21,6 +21,9 @@ const Portfolio = () => {
                 </a>
                 <div className="swappaws-description">
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
               </div>
               <div className='teamtracker'>               
@@ -28,6 +31,9 @@ const Portfolio = () => {
                   <Image className="teamtracker-logo" src={team} width={223} height={110}/>
                 </a>
                 <div className="teamtracker-description">
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                 </div>
               </div>
